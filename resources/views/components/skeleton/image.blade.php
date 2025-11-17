03a3d9f9ccb22efc46e09675dde13ea035e6d0e6
bluenoe <baokhanh.dev281@gmail.com>
2025-11-17 08:48:45 +0700

    style(views): update skeleton component background colors
    Replace gray-700 with ash color in skeleton components for better visual consistency

--- resources/views/components/skeleton/image.blade.php
+++ resources/views/components/skeleton/image.blade.php
@@ -1,2 +1,2 @@
 @props(['class' => 'aspect-[4/3]'])
-<div class="{{ $class }} bg-gray-700 animate-pulse"></div>
+<div class="{{ $class }} bg-ash animate-pulse"></div>
--- resources/views/components/skeleton/text.blade.php
+++ resources/views/components/skeleton/text.blade.php
@@ -1,6 +1,6 @@
 @props(['lines' => 2])
 <div class="space-y-2">
     @for ($i = 0; $i < $lines; $i++)
-        <div class="h-4 bg-gray-700 rounded animate-pulse"></div>
+        <div class="h-4 bg-ash rounded animate-pulse"></div>
     @endfor
 </div>
