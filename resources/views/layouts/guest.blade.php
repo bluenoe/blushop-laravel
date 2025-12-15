a50656cfc1c383c8b412bfbb375462e7bf4fe6bd
bluenoe <baokhanh.dev281@gmail.com>
2025-12-15 18:26:18 +0700

    refactor(assets): update favicon paths and replace logo format
    Update all favicon asset paths to include 'images/' prefix for consistency
    Replace blu-logo.png with blu-logo.jpg and update references

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,11 +13,11 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-96x96.png') }}" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}" />
-    <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}" />
-    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('images/favicon/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}" />
+    <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}" />
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
