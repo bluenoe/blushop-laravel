36bd145ba736fd0c78e2befe660f70a811175a8a
bluenoe <baokhanh.dev281@gmail.com>
2026-01-14 10:35:40 +0700

    format mail

--- resources/views/emails/orders/placed.blade.php
+++ resources/views/emails/orders/placed.blade.php
@@ -1,40 +1,40 @@
 <x-mail::message>
-    # 🎉 Cảm ơn bạn đã đặt hàng!
+# 🎉 Cảm ơn bạn đã đặt hàng!
 
-    Xin chào **{{ $customerName }}**,
+Xin chào **{{ $customerName }}**,
 
-    Chúng tôi đã nhận được đơn hàng của bạn và đang tiến hành xử lý.
+Chúng tôi đã nhận được đơn hàng của bạn và đang xử lý.
 
-    <x-mail::panel>
-        **Mã đơn hàng:** #{{ $orderId }}
-        <br>
-        **Tổng thanh toán:** {{ $totalPrice }}
-        <br>
-        **Địa chỉ nhận hàng:** {{ $shippingAddress }}
-    </x-mail::panel>
+<x-mail::panel>
+    **Mã đơn hàng:** #{{ $orderId }}
+    <br>
+    **Tổng thanh toán:** {{ $totalPrice }}
+    <br>
+    **Địa chỉ giao hàng:** {{ $shippingAddress }}
+</x-mail::panel>
 
-    ## 🛒 Chi tiết đơn hàng
+## 🛒 Chi tiết đơn hàng
 
-    <x-mail::table>
-        | Sản phẩm | SL | Đơn giá | Thành tiền |
-        |:--- |:---:|:---:|:---:|
-        @foreach ($orderItems as $item)
-        | **{{ $item->product->name ?? 'Sản phẩm' }}** | {{ $item->quantity }} | {{
-        number_format($item->price_at_purchase, 0, ',', '.') }}₫ | **{{ number_format($item->price_at_purchase *
-        $item->quantity, 0, ',', '.') }}₫** |
-        @endforeach
-    </x-mail::table>
+<x-mail::table>
+    | Sản phẩm | SL | Đơn giá | Thành tiền |
+    |:--- |:---:|:---:|:---:|
+    @foreach ($orderItems as $item)
+    | **{{ $item->product->name ?? 'Sản phẩm' }}** | {{ $item->quantity }} | {{
+    number_format($item->price_at_purchase, 0, ',', '.') }}₫ | **{{ number_format($item->price_at_purchase *
+    $item->quantity, 0, ',', '.') }}₫** |
+    @endforeach
+</x-mail::table>
 
-    <x-mail::button :url="route('orders.show', $orderId)" color="primary">
-        Xem chi tiết đơn hàng
-    </x-mail::button>
+<x-mail::button :url="route('orders.show', $orderId)" color="primary">
+    Xem đơn hàng của bạn
+</x-mail::button>
 
-    Nếu bạn có bất kỳ câu hỏi nào, vui lòng trả lời email này hoặc liên hệ hotline.
+Nếu bạn có bất kỳ câu hỏi nào, vui lòng liên hệ với chúng tôi qua email hoặc hotline.
 
-    Trân trọng,<br>
-    **{{ config('app.name') }}**
+Trân trọng,<br>
+**{{ config('app.name') }}**
 
-    <x-mail::subcopy>
-        Đây là email tự động, vui lòng không trả lời trực tiếp email này nếu không cần hỗ trợ.
-    </x-mail::subcopy>
+<x-mail::subcopy>
+    Đây là email tự động, vui lòng không trả lời trực tiếp email này.
+</x-mail::subcopy>
 </x-mail::message>
